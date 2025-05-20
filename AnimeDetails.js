691efb1ecb932ad5e91e475045e31e0f14207d12
AniWatch <axundovelmir31@gmail.com>
2025-05-20 15:47:07 +0400

    Update AnimeDetails.js

--- AnimeDetails.js
+++ AnimeDetails.js
@@ -1,36 +1,11 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import animeData from "../assets/animeData";
-
-function AnimeDetails() {
-  const { animeId } = useParams();
-  const anime = animeData.find((a) => a.id === animeId);
-
-  if (!anime) return <p className="p-4">Anime tapılmadı.</p>;
-
-  return (
-    <div className="p-4">
-      <h2 className="text-2xl mb-4">{anime.title}</h2>
-      <p>{anime.description}</p>
-      <div className="mt-4">
-        <h3 className="text-xl">Bölümlər:</h3>
-        <ul>
-          {anime.episodes.map((ep, index) => (
-            <li key={index}>
-              <a
-                href={ep.url}
-                className="text-blue-600 hover:underline"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {ep.title}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
-}
-
-export default AnimeDetails;
+<div className="mt-4">
+  <h3 className="text-xl mb-2">Bölüm:</h3>
+  <iframe
+    width="100%"
+    height="500"
+    src="https://www.youtube.com/embed/abc123"
+    title="Attack on Titan Bölüm 1"
+    frameBorder="0"
+    allowFullScreen
+  ></iframe>
+</div>
